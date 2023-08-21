b17f14209b75214e52bfcf9dff3ec50348cde62c
devadrsilva <adriano.mssilva1@gmail.com>
2023-08-21 16:53:51 -0300

    quase lá

---
+++ conta.rb
@@ -0,0 +1,27 @@
+class Conta
+   attr_accessor :nome, :saldo
+   @saldo = 0 
+   def initialize ( nome, saldo)
+      @nome = nome 
+      @saldo = saldo
+   end
+
+   def saldo_conta
+   puts "saldo da conta é #{@saldo}"
+      
+   end
+
+   def deposito (valor_dep)
+     @saldo += valor_dep
+   end
+
+   def debitar (valor_deb)
+      @saldo -=valor_deb
+   end
+
+   def trans (conta_destino, valor)
+      debitar(valor)
+      conta_destino.deposito(valor)
+   end
+end
+
